10dd3d9f48042b72f165ef1244933b9c8d576d4d
Markus Tacker <m@coderbyheart.com>
2020-01-15 11:49:46 +0100

    First implementation

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs')
+const os = require('fs')
+const { execSync } = require('child_process')
+
+const exec = cmd => {
+    console.log('Running: ', cmd)
+    const res = execSync(cmd, { cwd: process.cwd() })
+    console.log('Result:', res)
+}
 
 try {
     const context = github.context;
@@ -20,11 +29,63 @@ try {
         return;
     }
 
-    const { title, body, labels } = payload.issue;
+    const { number, title, body, labels } = payload.issue;
+
+    if (!labels.find(({ name }) => name === 'Registration')) {
+        core.debug(
+            'The issue does not have the registration label, skipping.'
+        );
+        return;
+    }
 
     console.log('title', title)
     console.log('body', body)
-    console.log('labels', JSON.stringify(labels, undefined, 2))
+    const { name, pronouns, image, twitter, linkedin, homepage } = JSON.parse(body.match(/\`\`\`json([^\`]+)```/m)[1])
+    if (!name) {
+        throw new Error(`Registration issue #${number} has not name!`)
+    }
+
+    const data = {
+        name,
+        ...(pronouns ? { pronouns } : {}),
+        ...(image ? { image } : {}),
+        ...(twitter ? { twitter } : {}),
+        ...(linkedin ? { linkedin } : {}),
+        ...(homepage ? { homepage } : {}),
+    }
+    //
+    console.log('cwd', process.cwd())
+    const outfile = `${process.cwd()}/_participants/${name.replace(/\s/g, '')}.md`
+    fs.writeFileSync(
+        outfile,
+        `---
+        ${Object.entries(data).map(([k, v]) => `${k}: ${v}`).join('\n')}
+        ---`,
+        'utf-8'
+    )
+    console.log(`${outfile} written ...`)
+    exec(`git add ${outfile}`)
+    exec(`git checkout -b registration-${number}`)
+    exec(`git commit -m "Registration for ${name}"`)
+    exec(`git push -u origin registration-${number}`)
+    const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
+    console.log('Creating PR')
+    octokit.pulls
+        .create({
+            base: 'gh-pages',
+            head: `registration-${number}`,
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            title: `Registration for ${name}`,
+            body: 'This pull-request was created automatically.',
+            maintainer_can_modify: true,
+        })
+        .then(pr => {
+            console.log(`PR ${pr.data.id} created!`)
+        })
+        .catch(error => {
+            core.setFailed(error.message)
+        })
 } catch (error) {
     core.setFailed(error.message);
 }
