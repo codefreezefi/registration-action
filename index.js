04690a049793050daf8332a2fe7c9dffcb162a43
Markus Tacker <m@coderbyheart.com>
2020-01-15 12:01:29 +0100

    fix: dont set git user info

--- index.js
+++ index.js
@@ -55,15 +55,15 @@ try {
     const outfile = `${process.cwd()}/_participants/${name.replace(/\s/g, '')}.md`
     fs.writeFileSync(
         outfile,
-        `---
-        ${Object.entries(data).map(([k, v]) => `${k}: ${v}`).join('\n')}
-        ---`,
+        [
+            '---',
+            Object.entries(data).map(([k, v]) => `${k}: ${v}`).join('\n'),
+            '---',
+        ].join('\n'),
         'utf-8'
     )
     console.log(`${outfile} written ...`)
     exec(`cat ${outfile}`)
-    exec(`git config --global user.email registrations@codefreeze.fi`)
-    exec(`git config --global user.name "Codefreeze Registrations"`)
     exec(`git add ${outfile}`)
     exec(`git checkout -b registration-${number}`)
     exec(`git commit -m "Registration for ${name}"`)
